b792a32b035d840f8eb96e1d5c647f6ee99ef924
gregory <wiznergregory@gmail.com>
2022-04-14 19:20:27 -0400

    Exercize3

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+double userIn = 0;
+
+Console.WriteLine("Pick a number:");
+string userInput = Console.ReadLine();
+double.TryParse(userInput, out userIn);
+Console.WriteLine();
+Console.WriteLine(userIn + .5);
+Console.ReadLine();
